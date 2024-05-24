1827cfe7e8c3299611992c130ec1b9908186c6b9
Victor Tantillo <v.tantillo3@gmail.com>
2024-05-24 08:19:44 -0600

    Register account

--- apps/st-web/src/app/(auth)/login/login-form.tsx
+++ apps/st-web/src/app/(auth)/login/login-form.tsx
@@ -75,7 +75,7 @@ export function LoginForm() {
 
         <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?{' '}
-          <Link href="#" className="underline">
+          <Link href="/register" className="underline">
             Sign up
           </Link>
         </div>
