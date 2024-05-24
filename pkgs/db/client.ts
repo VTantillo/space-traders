a3f801c420715188c4196ee867cd07be503ee558
Victor Tantillo <v.tantillo3@gmail.com>
2024-05-23 20:04:42 -0600

    Added DB

---
+++ pkgs/db/client.ts
@@ -0,0 +1,6 @@
+import Database from 'better-sqlite3'
+import { drizzle } from 'drizzle-orm/better-sqlite3'
+import 'dotenv/config'
+
+const sqlite = new Database(process.env.DB_PATH)
+export const db = drizzle(sqlite)
---
+++ pkgs/db/schema.ts
@@ -0,0 +1,43 @@
+import {
+  integer,
+  sqliteTable,
+  text,
+  uniqueIndex,
+} from 'drizzle-orm/sqlite-core'
+import { ulid } from 'ulid'
+
+export const dbUser = sqliteTable(
+  'user',
+  {
+    id: text('id')
+      .$defaultFn(() => ulid())
+      .primaryKey()
+      .notNull(),
+    username: text('username').notNull(),
+    password: text('password').notNull(),
+    isAdmin: integer('is_admin', { mode: 'boolean' }).default(false).notNull(),
+  },
+  (u) => {
+    return {
+      usernameIndex: uniqueIndex('username_idx').on(u.username),
+    }
+  },
+)
+export type User = typeof dbUser.$inferSelect
+export type NewUser = typeof dbUser.$inferInsert
+
+export const dbSession = sqliteTable('session', {
+  sessionToken: text('session_token').primaryKey().notNull(),
+  userId: text('user_id')
+    .references(() => dbUser.id, {
+      onDelete: 'cascade',
+    })
+    .notNull(),
+  issuedAt: integer('issued_at', { mode: 'timestamp' })
+    .$defaultFn(() => new Date())
+    .notNull(),
+  revokedAt: integer('revoked_at', { mode: 'timestamp' }),
+  expiresAt: integer('expires_at', { mode: 'timestamp' }),
+})
+export type Session = typeof dbSession.$inferSelect
+export type NewSession = typeof dbSession.$inferInsert
